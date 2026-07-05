11f93809f9113f5558408ea09de39ee4d6d077d5
Bob _ <baoyunfan0101@gmail.com>
2026-07-05 22:56:16 +0800

    chore: bump version to v0.3.0

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from .model import EngineAction, EngineEvent, Failure, Item, Request, Response
 from .registry import ComponentSpec
 from .spider import Spider
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 __all__ = [
     'Engine',
